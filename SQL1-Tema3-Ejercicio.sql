d4da509cc44b740d225891e4391c4a3abeb4ec92
Fernando Arellano <f.arellano919@gmail.com>
2021-10-21 02:01:58 -0300

    ordeno codigo

--- SQL1-Tema3-Ejercicio.sql
+++ SQL1-Tema3-Ejercicio.sql
@@ -1,27 +1,35 @@
 /* CONSULTA 1 */
 
-SELECT nombre, apellido, fecha_nacimiento FROM
-profesor ORDER BY fecha_nacimiento;
+SELECT nombre, apellido, fecha_nacimiento 
+FROM profesor 
+ORDER BY fecha_nacimiento;
 
 /* CONSULTA 2 */
 
-SELECT * FROM profesor WHERE salario >= 65000;
+SELECT * 
+FROM profesor 
+WHERE salario >= 65000;
 
 /* CONSULTA 3 */
 
-SELECT * FROM profesor WHERE fecha_nacimiento BETWEEN
-'1980-01-01' AND '1989-12-31';
+SELECT * 
+FROM profesor 
+WHERE fecha_nacimiento BETWEEN '1980-01-01' AND '1989-12-31';
 
 /* CONSULTA 4 */
 
-SELECT * FROM profesor LIMIT 5;
+SELECT * 
+FROM profesor 
+LIMIT 5;
 
 /* CONSULTA 5 */
 
-SELECT * FROM profesor WHERE apellido LIKE "P%";
+SELECT * 
+FROM profesor 
+WHERE apellido LIKE "P%";
 
 /* CONSULTA 6 */
 
-SELECT * FROM profesor WHERE fecha_nacimiento
-between '1980-01-01' and '1989-12-31' AND salario >
-80000;
+SELECT * 
+FROM profesor 
+WHERE fecha_nacimiento BETWEEN '1980-01-01' AND '1989-12-31' AND salario > 80000;
